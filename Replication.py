cb5f16c7d444caef453e0e31ebc4884b64f91508
AMALOVLO <165968065+AMALOVLO@users.noreply.github.com>
2024-04-09 10:45:27 +1000

    Create Replication.py

---
+++ Replication.py
@@ -0,0 +1,8 @@
+replicate = 0 
+#the code above sets the variable which will be used to open files in the future
+while (replicate == 0):
+  with open('REPLICATE', 'a') as :
+    the_file.write('e')
+
+  
+  file.close()
